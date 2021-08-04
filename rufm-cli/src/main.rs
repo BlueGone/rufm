7b894089005e799ec9bd23f5cf856406299d3851
Thomas Plaçais <thomas.placais@protonmail.com>
2021-08-04 18:16:14 +0200

    :construction: (cli): Add basic arguments parsing

--- rufm-cli/src/main.rs
+++ rufm-cli/src/main.rs
@@ -1,3 +1,37 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Database file path
+    #[structopt(short, long, default_value = "~/.rufm.db")]
+    database_path: String,
+
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Create, list, and manage accounts
+    #[structopt()]
+    Accounts(AccountsCommand),
+}
+
+#[derive(Debug, StructOpt)]
+enum AccountsCommand {
+    /// Create an account
+    #[structopt()]
+    Create {
+        /// Account name
+        name: String,
+    },
+    /// List all account
+    #[structopt()]
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Opt = Opt::from_args();
+
+    println!("{:?}", opt);
 }
